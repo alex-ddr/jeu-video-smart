703e76a5ff2dcc212190335010a7fe9c55877a33
Aloha909 <81017787+Aloha909@users.noreply.github.com>
2026-04-28 09:55:47 +0200

    collisions ligne 2d

--- entities/player/line_2d.gd
+++ entities/player/line_2d.gd
@@ -3,9 +3,11 @@ extends Line2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var newStaticBody = StaticBody2D.new()
+	add_child(newStaticBody)
 	for i in points.size() - 1:
 		var new_shape = CollisionShape2D.new()
-		$StaticBody2D.add_child(new_shape)
+		newStaticBody.add_child(new_shape)
 		var segment = SegmentShape2D.new()
 		segment.a = points[i]
 		segment.b = points[i + 1]
