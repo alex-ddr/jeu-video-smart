5da66d40d37f1a971e795d6d80c949cfafb7ab42
alex-ddr <atrdidier@gmail.com>
2026-05-04 11:11:15 +0200

    player speed

--- entities/player/player_duo.gd
+++ entities/player/player_duo.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 # --------------------------- Constants ---------------------------
 const TILE_SIZE: float = Global.TILE_SIZE
-const MAX_SPEED: float = TILE_SIZE * 3.125
-const MIN_SPEED: float = TILE_SIZE * 0.625
+const MAX_SPEED: float = TILE_SIZE * 2.4
+const MIN_SPEED: float = TILE_SIZE * 1
 const ACCELERATION: float = TILE_SIZE * 12.5
 const FRICTION: float = TILE_SIZE * 12.5
 const WEIGHT_CURVE: float = 2.0
