2b50770564977b1fc347b880538ed4d511bd85ea
Raphaël <letourneur.raphael@gmail.com>
2026-05-04 18:40:42 +0200

    ajout d'un système de vie (nombre de balles restantes)

--- levels/base_level.gd
+++ levels/base_level.gd
@@ -24,11 +24,16 @@ func _physics_process(_delta: float) -> void:
 func _spawn_at_checkpoint(id: int) -> void:
 	for cp in get_tree().get_nodes_in_group("checkpoints"):
 		if cp.checkpoint_id == id:
-			player_duo.p1.global_position = cp.global_position + Vector2(-60, 0)
-			player_duo.p2.global_position = cp.global_position + Vector2(60, 0)
+			# Pour les joueurs (qui sont des CharacterBody2D), ça marche normalement :
+			player_duo.p1.global_position = cp.global_position + Vector2(-200, 0)
+			player_duo.p2.global_position = cp.global_position + Vector2(200, 0)
 			player_duo.p1.velocity = Vector2.ZERO
 			player_duo.p2.velocity = Vector2.ZERO
-			ball.global_position = cp.global_position + Vector2(0, -180)
-			ball.linear_velocity = Vector2.ZERO
-			ball.angular_velocity = 0.0
-			return
+
+			# POUR LA BALLE (RigidBody2D) : On doit utiliser set_deferred pour forcer la physique
+			var new_ball_pos = cp.global_position + Vector2(0, -180)
+			ball.set_deferred("global_position", new_ball_pos)
+			ball.set_deferred("linear_velocity", Vector2.ZERO)
+			ball.set_deferred("angular_velocity", 0.0)
+			
+		return
