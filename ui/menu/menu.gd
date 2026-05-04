68661e4b798bb77fbbf11361c71d0361b49bdf6e
robin <robin.renous83@gmail.com>
2026-05-04 16:07:55 +0200

    fix: menus

--- ui/menu/menu.gd
+++ ui/menu/menu.gd
@@ -4,7 +4,7 @@ func _on_play_pressed() -> void:
 	GameManager.start_game(0)
 
 func _on_levels_pressed() -> void:
-	get_tree().change_scene_to_file("res://ui/levels/levels.tscn")
+	get_tree().change_scene_to_file("res://ui/menu/levels.tscn")
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://ui/credits/credits.tscn")
