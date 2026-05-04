02c41479665c8dc24de5f3aa6e88ac835555e721
robin <robin.renous83@gmail.com>
2026-05-04 08:29:57 +0200

    taille menus

--- ui/credits/crédits.gd
+++ ui/credits/crédits.gd
@@ -2,14 +2,18 @@ extends Control
 
 # Un tableau avec les noms de votre équipe
 const EQUIPE = [
-	{"nom": "Raphael Letourneur", "role": "Gameplay"},
-	
+	{"nom": "Raphael Letourneur", "role": "r"},
+	{"nom": "Maxence Heurtault", "role": "r"},
+	{"nom": "Alexandre Didier", "role": "r"},
+	{"nom": "Alois Pinto", "role": "r"},
+	{"nom": "Hugo Marin", "role": "r"},
+	{"nom": "Robin Renous", "role": "r"}
 ]
 
 func _ready() -> void:
 	for membre in EQUIPE:
 		var label = Label.new()
-		label.text = "%s — %s" % [membre["nom"], membre["role"]]
+		label.text = "%s" % [membre["nom"]]
 		$CenterContainer/VBoxContainer/VBoxContainer.add_child(label)
 
 func _on_retour_pressed() -> void:
