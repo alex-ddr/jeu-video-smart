c00a2dfbd974ab6245acf8ddee258b24a8d860a0
xamenss <maxenceheurtault04@gmail.com>
2026-05-05 10:45:45 +0200

    merge modif d'alex

--- entities/cactus/cactus.gd
+++ entities/cactus/cactus.gd
@@ -1,8 +1,7 @@
 extends Area2D
 
 func _on_body_entered(body):
-	# On vérifie si c'est bien le joueur (groupe "player")
-	if body.is_in_group("player"):
-		print("Mort par cactus !")
-		# On demande de recharger la scène de manière différée
-		get_tree().call_deferred("reload_current_scene")
+	# On vérifie si c'est le joueur ET s'il possède la fonction lose_life
+	if body.has_method("lose_life"):
+		print("🌵 Le cactus a piqué la balle !")
+		body.lose_life()
--- entities/death_zone/death_zone.gd
+++ entities/death_zone/death_zone.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
-func _on_body_entered(body: Node) -> void:
+func _on_body_entered(body):
 	if body.is_in_group("player"):
 		get_tree().reload_current_scene()
+	# On vérifie si c'est le joueur ET s'il possède la fonction lose_life
+	if body.has_method("lose_life"):
+		print("🌵 Le cactus a piqué la balle !")
+		body.lose_life()
