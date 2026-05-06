9d8feba5dfd6bdddc11693b0d65c17b860285ad5
xamenss <maxenceheurtault04@gmail.com>
2026-05-06 15:50:36 +0200

    fix bug niveau 1 et ball lose life

--- entities/ball/ball.gd
+++ entities/ball/ball.gd
@@ -71,18 +71,34 @@ func _on_ground_detector_body_entered(body: Node) -> void:
 			lose_life()
 		
 func lose_life() -> void:
-	Global.current_lives -= 1
-	Global.lives_changed.emit()
+	if is_invincible:
+		return
+		
+	# On ne retire une vie QUE si on a dépassé le checkpoint de départ (ID 0)
+	if Global.current_checkpoint_id > 0:
+		Global.current_lives -= 1
+		Global.lives_changed.emit()
+		print("Vie perdue ! Nouveau total : ", Global.current_lives)
+	else:
+		print("Respawn au checkpoint 0 : aucune vie perdue.")
+	
+	# Vérification du Game Over
 	if Global.current_lives <= 0:
 		Global.current_lives = Global.max_lives
-		get_tree().call_deferred("reload_current_scene")
+		Global.current_checkpoint_id = 0 # Sécurité : reset en cas de retour forcé au menu
+		GameManager.start_game()
 	else:
+		# Procédure de respawn classique
 		linear_velocity = Vector2.ZERO
 		angular_velocity = 0.0
 		is_invincible = true
+
 		var level = get_tree().current_scene
 		if level.has_method("_spawn_at_checkpoint"):
 			level._spawn_at_checkpoint()
 		else:
-			print("pas de checkpoint")
+			print("Attention: La scène actuelle n'a pas de méthode '_spawn_at_checkpoint'")
+			
+		# Petit délai d'invincibilité (1 seconde) pour éviter de mourir en boucle au respawn
+		await get_tree().create_timer(1.0).timeout
 		is_invincible = false
